221ca342dfcb2a77f073430fed9cf420160e6d72
LazyManish <mbhattarai900@gmail.com>
2025-02-17 16:20:20 +0545

    added logout view

--- user/api/views.py
+++ user/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status,generics
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny,IsAuthenticated
 from user.models import User
 from .permissions import IsSuperAdmin
 
@@ -47,4 +47,24 @@ class UserLoginView(APIView):
                  }, status= status.HTTP_200_OK)
             return Response({'error': 'Invalid credentials'}, status= status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST) 
+
+class UserLogoutView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        try:
+
+            refresh_token = request.data.get('refresh_token')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({"message":"user logged out successfully."},status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_400_BAD_REQUEST)
+        
+        
+
 
